7492685623b4b4202c873d3033bd01f9aa9028f7
W3ndig0 <70271139+W3ndig0u0@users.noreply.github.com>
2022-09-25 17:21:03 +0200

    as

--- veckoDagen.py
+++ veckoDagen.py
@@ -57,37 +57,3 @@ elif weekday == 5:
     print("It is a Thursday")
 elif weekday == 6:
     print("It is a Friday")
-
-
-# year = int(input("Year: "))
-# while year < 1583 or year > 9999:
-#     print("Out of allowed range 1583 to 9999")
-#     year = int(input("Year: "))
-
-# month = int(input("Month: "))
-# while month < 1 or month > 12:
-#     print("Out of allowed range 1 to 12")
-#     month = int(input("Month: "))
-
-# day = int(input("Day: "))
-# if year % 400 == 0 or (year % 4 == 0 and year % 100 != 0):
-#     while month == 2 and day < 1 or day > 29:
-#         print("Out of allowed range 1 to 29")
-#         day = int(input("Day: "))
-
-# while month in (1, 3, 5, 7, 8, 10, 12) and day < 1 or day > 31:
-#     print("Out of allowed range 1 to 32")
-#     day = int(input("Day: "))
-
-# while month in (4, 6, 9, 11) and day < 1 or day > 30:
-#     print("Out of allowed range 1 to 30")
-#     day = int(input("Day: "))
-
-# else:
-#     while month == 2 and day < 1 or day > 28:
-#         print("Out of allowed range 1 to 28")
-#         day = int(input("Day: "))
-
-# while month in (1, 3, 5, 7, 8, 10, 12) and day < 1 or day > 31:
-#     print("Out of allowed range 1 to 31")
-#     day = int(input("Day: "))
